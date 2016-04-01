05b93a653cbfe678f5a70fef52d1cda157e6895e
mathferret1013 <eregina92@gmail.com>
2016-04-01 00:18:24 -0400

    First commit

---
+++ Supercluster.MTree/Supercluster.MTree/MRoutingNode.cs
@@ -0,0 +1,27 @@
+﻿namespace Supercluster.MTree
+{
+    public class MRoutingNode<T>
+    {
+        // May need a point data type as we did in the KDTree
+        // May also need to define a TDimension type
+
+        public T Node;
+
+        public MRoutingNode<T> CoveringTreeRoot;
+
+        public double CoveringRadius;
+
+        public double DistanceFromParent;
+    }
+
+
+    public class MLeafNode<T>
+    {
+        // May need a point data type as we did in the KDTree
+        // May also need to define a TDimension type
+
+        public T Node;
+
+        public double DistanceFromParent;
+    }
+}
